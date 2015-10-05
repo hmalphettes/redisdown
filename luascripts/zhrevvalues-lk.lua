70a73b3a55537ae5827fd9816f13ef0c73f5ffb0
Hugues Malphettes <hmalphettes@gmail.com>
2015-10-05 11:37:25 +0800

    #19 Fix the value streams

--- luascripts/zhrevvalues-lk.lua
+++ luascripts/zhrevvalues-lk.lua
@@ -4,15 +4,12 @@
  Make a reverse range by lex on an order map and return the values
  Note that the last returned value is the key of the last value. Used for the next iteration.
 
- `redis-cli EVAL "$(cat zhrevvalues.lua)" 1 _redisdown_test_db_:10 ZRANGEBYLEX - +`
+ `redis-cli EVAL "$(cat zhrevvalues.lua)" 1 _redisdown_test_db_:10 - +`
 ]]
 local keys = redis.call('zrevrangebylex', KEYS[1]..':z', unpack(ARGV))
 if #keys == 0 then
   return keys
 end
-local vals = redis.call('hmget',KEYS[1]..':h',unpack(keys))
-if vals == nil then
-  return nil
-end
-vals[#keys+1] = keys[#keys]
-return vals
+local values = redis.call('hmget', KEYS[1]..':h', unpack(keys))
+values[#values+1] = keys[#keys]
+return values
--- luascripts/zhvalues-lk.lua
+++ luascripts/zhvalues-lk.lua
@@ -4,15 +4,12 @@
  Make a range query by lex on an ordered map and return the values
  Note that the last returned value is the key of the last value. Used for the next iteration.
 
- `redis-cli EVAL "$(cat zhvalues.lua)" 1 _redisdown_test_db_:10 ZRANGEBYLEX - +`
+ `redis-cli EVAL "$(cat zhvalues-lk.lua)" 1 _redisdown_test_db_:10 - +`
 ]]
 local keys = redis.call('zrangebylex', KEYS[1]..':z', unpack(ARGV))
 if #keys == 0 then
   return keys
 end
-local vals redis.call('hmget',KEYS[1]..':h',unpack(keys))
-if vals == nil then
-  return nil
-end
-vals[#keys+1] = keys[#keys]
-return vals
+local values = redis.call('hmget', KEYS[1]..':h', unpack(keys))
+values[#values+1] = keys[#keys]
+return values
